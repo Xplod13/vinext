60a166c1e2a4b0c34f4436ea733750d01320a11b
James <james@eli.cx>
2026-05-26 13:48:27 +0100

    adapt for workers

--- tests/fixtures/ecosystem/payload-cms/src/payload.config.ts
+++ tests/fixtures/ecosystem/payload-cms/src/payload.config.ts
@@ -4,7 +4,6 @@ import { sqliteD1Adapter } from '@payloadcms/db-d1-sqlite'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import { CloudflareContext, getCloudflareContext } from '@opennextjs/cloudflare'
 import { GetPlatformProxyOptions } from 'wrangler'
 import { r2Storage } from '@payloadcms/storage-r2'
 
@@ -16,7 +15,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const realpath = (value: string) => (fs.existsSync(value) ? fs.realpathSync(value) : undefined)
 
-const isCLI = process.argv.some((value) => realpath(value).endsWith(path.join('payload', 'bin.js')))
+const isCLI = process.argv.some((value) => realpath(value)?.endsWith(path.join('payload', 'bin.js')))
 const isProduction = process.env.NODE_ENV === 'production'
 
 const createLog =
@@ -41,8 +40,8 @@ const cloudflareLogger = {
 
 const cloudflare =
   isCLI || !isProduction
-    ? await getCloudflareContextFromWrangler()
-    : await getCloudflareContext({ async: true })
+    ? await getCloudflareContext()
+    : await getCloudflareContext()
 
 export default buildConfig({
   admin: {
@@ -69,13 +68,6 @@ export default buildConfig({
   ],
 })
 
-// Adapted from https://github.com/opennextjs/opennextjs-cloudflare/blob/d00b3a13e42e65aad76fba41774815726422cc39/packages/cloudflare/src/api/cloudflare-context.ts#L328C36-L328C46
-function getCloudflareContextFromWrangler(): Promise<CloudflareContext> {
-  return import(/* webpackIgnore: true */ `${'__wrangler'.replaceAll('_', '')}`).then(
-    ({ getPlatformProxy }) =>
-      getPlatformProxy({
-        environment: process.env.CLOUDFLARE_ENV,
-        remoteBindings: isProduction,
-      } satisfies GetPlatformProxyOptions),
-  )
+function getCloudflareContext(): Promise<CloudflareContext> {
+	return import(/* webpackIgnore: true */ `${'__cloudflare:workers'.replaceAll('_', '')}`);
 }
